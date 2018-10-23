3bfb28f439d2db02d0dcc5a61c1bb50dd5300ab5
黄璞 <awsomekde@gmail.com>
2018-10-23 09:04:23 +0800

    Update tplink_helper.py

--- tplink_helper.py
+++ tplink_helper.py
@@ -20,14 +20,14 @@ headers = {
     "Referer": "http://192.168.0.1/userRpm/StatusRpm.htm",
     "Accept-Encoding": "gzip, deflate",
     "Accept-Language": "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7,bg;q=0.6,la;q=0.5",
-    "Cookie": "Authorization=你的Authorization值"
+    "Cookie": "Authorization=*你的authorization值*"
 }
 
 statusUrl = "http://" + ip + "/userRpm/StatusRpm.htm"
 
 loginUrl = "http://" + ip + "/userRpm/WanCfgRpm_8021X.htm" \
                             "?wantype=3" \
-                            "&Ieee802_1xName=你的8021x账号" \
+                            "&Ieee802_1xName=*你的宽带账户*" \
                             "&Ieee802_1xPwd=Hello123World" \
                             "&dialMode=2" \
                             "&Login=%B5%C7+%C2%BC&IpType=1" \
@@ -35,14 +35,28 @@ loginUrl = "http://" + ip + "/userRpm/WanCfgRpm_8021X.htm" \
                             "&sta_mask=" \
                             "&sta_gateway=" \
                             "&mtu=1500" \
-                            "&ieeeType=3" \
-                            "&IeeeReqFlag=0"
+                            "&ieeeType=3"
+#logoutUrl = "http://" + ip + "/userRpm/StatusRpm.htm?Logout=%CD%CB%20%B3%F6&wan=1"
+logoutUrl = "http://" + ip + "/userRpm/WanCfgRpm_8021X.htm" \
+                             "?wantype=3" \
+                             "&Ieee802_1xName=*你的宽带账户*" \
+                             "&Ieee802_1xPwd=Hello123World" \
+                             "&Logout=%CD%CB+%B3%F6" \
+                             "&IpType=1" \
+                             "&sta_ip=" \
+                             "&sta_mask=" \
+                             "&sta_gateway=" \
+                             "&mtu=1500" \
+                             "&ieeeType=3" \
+                             "&IeeeReqFlag=0"
 
 
 def get_url(url_arg):
     if url_arg == "get_status":
         req = urllib.request.Request(statusUrl, None, headers)
     elif url_arg == "login":
+        //先登出，再登入，防止特殊情况
+        req = urllib.request.Request(logoutUrl, None, headers)
         req = urllib.request.Request(loginUrl, None, headers)
     else:
         # default, should never reach here
